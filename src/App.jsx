5790b61a5283e00e8fe6dd0507b67d9404431f2f
Nyuykighan Jane <nyuykighanjane@gmail.com>
2025-04-09 17:26:43 +0100

    working on the web view

--- src/App.jsx
+++ src/App.jsx
@@ -51,7 +51,7 @@ const App = () => {
           {['home', 'skills', 'projects', 'contact'].map((item) => (
             <li
               key={item}
-              className={`p-2 cursor-pointer ${activeSection === item ? 'text-[#e03f4f] font-bold' : 'hover:text-[#e03f4f]'}`}
+              className={`p-2 cursor-pointer ${activeSection === item ? 'text-[#cf3443] font-bold' : 'hover:text-[#cf3443]'}`}
               onClick={() => handleScroll(item)}
             >
               {item.charAt(0).toUpperCase() + item.slice(1)}
@@ -63,11 +63,11 @@ const App = () => {
 
         {/* Home  */}
         <div>
-          <div className="flex flex-col justify-between">
+          <div className="flex flex-col justify-between lg:flex-row p-10">
             <div className="flex items-center justify-center pb-4">
-              <img src="src/assets/image.png" alt="image" className="rounded-full border border-black object-cover w-80 h-80" />
+              <img src="src/assets/image.png" alt="image" className="rounded-full border object-cover w-80 h-80 " />
             </div>
-            <div>
+            <div className="flex flex-col justify-center lg:ml-12 ">
               <h2 className="text-center pb-4 font-extrabold text-4xl">
                 Hi, I'm <span className="text-[#7a141e]">Jane-Mary</span>
               </h2>
@@ -93,10 +93,10 @@ const App = () => {
         </div>
 
         {/* Skills */}
-        <div id="skills" className="pb-6">
+        <div id="skills" className="pb-6 mb-4">
           <div className="bg-white rounded-md flex items-center justify-center p-8">
             <div className="max-w-5xl w-full mx-auto">
-              <h1 className="text-5xl text-center text-black mb-12 font-bold">
+              <h1 className="text-4xl text-center text-[#7a141e] mb-12 font-bold">
                 My Skills
               </h1>
               <div className="flex flex-wrap justify-center gap-10">
@@ -216,19 +216,15 @@ const App = () => {
         </div>
 
         {/* Projects*/}
-        <div id="projects" className="pb-6">
-          <h1 className="text-center font-bold text-3xl">Projects</h1>
-          <div className="pb-4">
-            <div className="flex items-center justify-center py-4">
-              <img
-                src="src/assets/moon.jpg"
-                alt="comfy"
-                className="rounded-2xl w-full shadow-lg "
-              />
+        <div id="projects" className="mb-20">
+          <h1 className="text-center font-bold text-4xl mb-10 text-[#7a141e]">Projects</h1>
+          <div className="flex flex-wrap items-center justify-center gap-10">
+            <div className=" ">
+              <img src="src/assets/moon.jpg" alt="comfy" className="rounded-2xl w-96 shadow-lg "/>
             </div>
             <div className="">
-              <h1 className="text-base font-bold">Comfy Homes</h1>
-              <p className="pb-1">
+              <h1 className="font-bold text-3xl">Comfy Homes</h1>
+              <p className="pb-1 w-96">
                 A websites where you can get easy, affordable and applicable
                 designs for your home.
               </p>
@@ -237,17 +233,13 @@ const App = () => {
               </button>
             </div>
           </div>
-          <div className="2">
-            <div className="flex items-center justify-center py-4">
-              <img
-                src="src/assets/moon.jpg"
-                alt="events"
-                className="rounded-2xl w-full shadow-lg"
-              />
+          <div className="flex flex-wrap flex-row-reverse items-center justify-center mt-9 gap-10">
+            <div className="">
+              <img src="src/assets/moon.jpg" alt="events" className="rounded-2xl w-96 shadow-lg0"/>
             </div>
             <div className="">
-              <h1 className="text-base font-bold">Aura Creations</h1>
-              <p className="pb-1">
+              <h1 className="pb-3 font-bold text-3xl">Aura Creations</h1>
+              <p className="pb-5 w-96">
                 A platform where event planners can come together and display
                 their ideas to customers giving them varities to choose from.
               </p>
@@ -264,14 +256,14 @@ const App = () => {
           id="contact"
           className=" mx-auto relative overflow-hidden  p-8 rounded-lg bg-white"
         >
-          <h2 className="text-2xl font-bold text-[#7a141e] mb-6 text-center">
+          <h2 className="text-center font-bold text-4xl text-[#7a141e] mb-10">
             Contact Me
           </h2>
+          <div className=" flex flex-wrap justify-center gap-10 ">
           <div className="">
-          <div className="">
-            <img src="src/assets/Messages-rafiki.svg" alt="" />
+            <img src="src/assets/Messages-rafiki.svg" alt="" className="w-96 h-25"/>
           </div>
-          <div className="">
+          <div className="w-96">
           <form method="post" action="#">
             <div className="mb-4">
               <label
